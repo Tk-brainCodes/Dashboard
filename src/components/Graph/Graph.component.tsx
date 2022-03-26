a51d3e9f7d1c276b3079f81b5128c71d9d0a2d4c
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-26 23:39:33 +0100

    added line-graph

--- src/components/Graph/Graph.component.tsx
+++ src/components/Graph/Graph.component.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from "react";
-import './Graph.styles.scss';
-
+import React from "react";
+import "./Graph.styles.scss";
+import Linegraph from "./Linegraph/linegraph";
 
 const Graph = () => {
   return (
     <div className="Graph">
       <div className="Graph-component">
-        <div className="line-chart"></div>
-        
-        </div>      
+        <Linegraph />
+        <div className="Graph-aside">
+          <div className="card-1">
+            <div className="card-title">Average Response Time</div>
+            <div className="time">30 Mins</div>
+          </div>
+          <div className="card-2">
+            <div className="card-title"> Response Time</div>
+            <div className="time">1 Hour 30 Mins</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
