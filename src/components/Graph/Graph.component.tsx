37ca53c03c0fd8b9cb51d14bdcdd9bcbcd32dac3
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-28 15:14:45 +0100

    made quick fix to graph

--- src/components/Graph/Graph.component.tsx
+++ src/components/Graph/Graph.component.tsx
@@ -4,21 +4,14 @@ import Linegraph from "./Linegraph/linegraph";
 
 const Graph = () => {
   return (
-    <div className="Graph">
-      <div className="Graph-component">
-        <Linegraph />
-        <div className="Graph-aside">
-          <div className="card-1">
-            <div className="card-title">Average Response Time</div>
-            <div className="time">30 Mins</div>
-          </div>
-          <div className="card-2">
-            <div className="card-title"> Response Time</div>
-            <div className="time">1 Hour 30 Mins</div>
-          </div>
+    <>
+      {/*Make through json data and render all three components*/}
+      <div className="Graph">
+        <div className="Graph-component">
+          <Linegraph />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
