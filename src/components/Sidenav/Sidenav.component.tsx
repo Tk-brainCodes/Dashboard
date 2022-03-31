c1c6ebec0921326f43e44ae4df919f3c378b9db2
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-31 02:44:56 +0100

    added dropdown list

--- src/components/Sidenav/Sidenav.component.tsx
+++ src/components/Sidenav/Sidenav.component.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { MENU_DATA } from "./menudata";
 import ArrowRight from "../../assests/navicons/arrowright.svg";
 import "../../styles/layouts/_sidenav.styles.scss";
 
 const Sidenav = () => {
-  const [data, setData] =
-    useState<{ name: string; id: number; icon: string }[]>(MENU_DATA);
-
   return (
     <div className="side-nav">
       <div className="card-container">
@@ -20,16 +17,25 @@ const Sidenav = () => {
       </div>
       <div className="menu-list">
         <div className="nav-list">
-          {data.map((Link) => (
-            <div key={Link.id} className="nav-list-container">
-              <div className="list-icon">
-                <img className="image-icon" src={Link.icon} alt="icon" />
-                <div className="list-text">{Link.name}</div>
+          {MENU_DATA.map((list) => (
+            <>
+              <div key={list.id} className="nav-list-container">
+                <div className="list-icon">
+                  <img className="image-icon" src={list.icon} alt="icon" />
+                  <div className="list-text">{list.name}</div>
+                </div>
+                <div className="list-dropdown">
+                  <img src={ArrowRight} alt="arrow-dropdown" />
+                </div>
               </div>
-              <div className="list-dropdown">
-                <img src={ArrowRight} alt="arrow-dropdown" />
-              </div>
-            </div>
+              {list.items.map((item) => (
+                <div key={item.id} className="drop-down-container">
+                  <ul>
+                    <li>{item.name}</li>
+                  </ul>
+                </div>
+              ))}
+            </>
           ))}
         </div>
       </div>
